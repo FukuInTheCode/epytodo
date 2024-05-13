943a0c121a3a048dadccc223ed3b4500b39c1292
Tom C <tom.carlier@epitech.eu>
2024-05-14 00:01:54 +0200

    [feat] -->  add the check_user_by_id function without any code

--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -7,3 +7,7 @@ exports.get_all_users = (to_call) => {
 exports.get_all_user_todos = (email, to_call) => {
     return to_call();
 }
+
+exports.check_user_by_id = (req.params.id, to_call) => {
+    return to_call(84);
+};
