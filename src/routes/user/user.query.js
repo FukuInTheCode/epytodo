26cdc02db47dd0eabc6f928dcc7f738ebc32e8d4
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-14 00:22:35 +0200

    [feat] -->  added the function for retrieve user data

--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -1,7 +1,12 @@
 const db = require("../../config/db.js");
 
 exports.get_all_users = (to_call) => {
-    return to_call();
+    db.query("SELECT id, email, password, created_at, firstname, name FROM user", (err, result) => {
+        if (err)
+            to_call(err);
+        else
+            to_call(null, result);
+    });
 }
 
 exports.get_all_user_todos = (email, to_call) => {
