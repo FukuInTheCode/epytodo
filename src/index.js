da9ed9b06ca1e970272c3f5d9f607023c65ab60f
Tom C <tom.carlier@epitech.eu>
2024-05-13 22:02:29 +0200

    [refactor] -->  activate the user.js in the index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 const port = process.env.PORT || 3000;
-// require("./routes/user/user.js")(app);
+require("./routes/user/user.js")(app);
 // require("./routes/todos/todos.js")(app);
 // require("./routes/auth/auth.js")(app);
 
