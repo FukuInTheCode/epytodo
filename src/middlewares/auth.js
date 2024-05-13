0ac256d395d31721c2933b272b4154c824efc5f0
Tom C <tom.carlier@epitech.eu>
2024-05-13 18:08:51 +0200

    [feat] -->  add the auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const header = req.headers.authorization;
+
+    if (!header) {
+        res.status(498).json({"msg": "No token , authorization denied"});
+        return;
+    }
+    const token = header.split(' ')[1];
+    jwt.verify(token, process.env.SECRET, (err, user) => {
+    if (err) {
+        res.status(498).json({"msg": "Token is not valid"});
+        return;
+    }
+    next();
+    });
+};
