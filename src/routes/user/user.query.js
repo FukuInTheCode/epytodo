437735a4df603537a98f3d1efd8d243a8ac2cb56
Tom C <tom.carlier@epitech.eu>
2024-05-13 21:55:47 +0200

    [feat] -->  add the get_all_user_todos without any code

--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -3,3 +3,7 @@ const db = require("../../config/db.js");
 exports.get_all_users = (to_call) => {
     return to_call();
 }
+
+exports.get_all_user_todos = (email, to_call) => {
+    return to_call();
+}
