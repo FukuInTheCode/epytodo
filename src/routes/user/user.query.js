f12555c2aebda26a9c8ad4ef0dba8d5f13f52f1c
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-13 23:26:50 +0200

    [feat] -->  added the function for find if the email already exist

--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -1,7 +1,17 @@
 const db = require("../../config/db.js");
 
 exports.check_email = (email, to_call) => {
-    return to_call(0);
+    db.query("SELECT COUNT(*) AS count FROM user WHERE email = ?", [email], (err, result) => {
+        if (err)
+            to_call(84);
+        else {
+            const count = result[0].count;
+            if (count > 0)
+                to_call(1);
+            else
+                to_call(0);
+        }
+    });
 };
 
 exports.create_user = (email, firstname, name, password, to_call) => {
