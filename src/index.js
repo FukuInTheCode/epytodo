258b77a447c7d32fc911f78fbd4572a33b40a0f0
Tom C <tom.carlier@epitech.eu>
2024-05-13 21:09:02 +0200

    [fix] --> fix the index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var bcrypt = require('bcryptjs');
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 require("./routes/auth/auth.js")(app, bcrypt);
 // require("./routes/user/user.js")(app);
 // require("./routes/todos/todos.js")(app);
