acd93e8152881b7fba46ceae6ea0b07a5f33d690
Tom C <tom.carlier@epitech.eu>
2024-05-13 21:05:44 +0200

    [refactor] -->  add the function to be done in the user.query.js

---
+++ src/routes/user/user.query.js
@@ -0,0 +1,9 @@
+const db = require("../../config/db.js");
+
+exports.check_email = (email, to_call) => {
+    return;
+};
+
+exports.create_user = (email, firstname, name, password, to_call) => {
+    return;
+};
