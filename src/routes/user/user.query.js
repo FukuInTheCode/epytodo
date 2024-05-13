e23707883a2e683dcdc0a3ddea0e91674480c711
Tom C <tom.carlier@epitech.eu>
2024-05-13 21:18:48 +0200

    [refactor] -->  call the function in parameter to test the code

--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -1,9 +1,9 @@
 const db = require("../../config/db.js");
 
 exports.check_email = (email, to_call) => {
-    return;
+    return to_call(0);
 };
 
 exports.create_user = (email, firstname, name, password, to_call) => {
-    return;
+    return to_call(0);
 };
