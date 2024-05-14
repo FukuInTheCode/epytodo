e73af53473d4436c2d6fe3b2565f63ac12730b60
Tom C <tom.carlier@epitech.eu>
2024-05-14 10:27:26 +0200

    [refactor] -->  add the /todos/:id route

--- src/routes/todo/todo.js
+++ src/routes/todo/todo.js
@@ -10,4 +10,24 @@ module.exports = (app) => {
             res.status(200).json(result);
         });
     });
+
+    app.get("/todos/:id", auth, (req, res) => {
+        check_todo(req.params.id, (exist) => {
+            if (exist === 84) {
+                res.status(500).json({"msg": "Internal server error"});
+                return;
+            }
+            if (!exist) {
+                res.status(404).json({"msg": "Not fount"});
+                return;
+            }
+            get_todo_by_id(req.params.id, (result) => {
+                if (!result) {
+                    res.status(500).json({"msg": "Internal server error"});
+                    return;
+                }
+                res.status(200).json(result);
+            });
+        })
+    });
 };
