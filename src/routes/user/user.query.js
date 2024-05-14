2345151ef3e2be3fe0b97e448c51241543aa9c21
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-14 10:07:50 +0200

    [feat] -->  added the function for get the user by an id

--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -23,7 +23,12 @@ exports.check_user_by_id = (id, to_call) => {
 };
 
 exports.get_user_by_id = (id, to_call) => {
-    return to_call();
+    db.query("SELECT * FROM user WHERE id = ?", [id], (err, result) => {
+        if (err)
+            to_call(undefined);
+        else
+            to_call(result);
+    });
 };
 
 exports.check_user_by_email = (email, to_call) => {
@@ -31,7 +36,12 @@ exports.check_user_by_email = (email, to_call) => {
 };
 
 exports.get_user_by_email = (email, to_call) => {
-    return to_call();
+    db.query("SELECT * FROM user WHERE email = ?", [email], (err, result) => {
+        if (err)
+            to_call(undefined);
+        else
+            to_call(result);
+    });
 };
 
 
