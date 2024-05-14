4cf5707d28a69c80cde4476770a6b386a26dff3d
Tom C <tom.carlier@epitech.eu>
2024-05-14 11:53:48 +0200

    [refactor] -->  add the /todos/:id put route

--- src/routes/todo/todo.js
+++ src/routes/todo/todo.js
@@ -38,6 +38,10 @@ module.exports = (app) => {
         const user_id = req.body["user_id"];
         const status = req.body["status"];
 
+        if (!title || !desc || !due_time || !user_id || !status) {
+            res.status(498).json({"msg": "Bad parameter"});
+            return;
+        }
         add_todo(title, description, due_time, user_id, status, (id) => {
             if (id === -1) {
                 res.status(500).json({"msg": "Internal server error"});
@@ -49,6 +53,28 @@ module.exports = (app) => {
                     return;
                 }
                 res.status(200).json(result);
+            });
+        })
+    });
+
+    app.put("/todos/:id", auth, (req, res) => {
+        const title = req.body["title"];
+        const desc = req.body["description"];
+        const due_time = req.body["due_time"];
+        const user_id = req.body["user_id"];
+        const status = req.body["status"];
+
+        if (!title || !desc || !due_time || !user_id || !status) {
+            res.status(498).json({"msg": "Bad parameter"});
+            return;
+        }
+        check_todo_by_id(req.params.id, (exist) => {
+            update_todo_by_id(id, title, desc, due_time, user_id, status, (err) => {
+                if (err === 84) {
+                    res.status(500).json({"msg": "Internal server error"});
+                    return;
+                }
+                res.status(200).json({"title": title, "description": desc, "due_time": due_time, "user_id": user_id, "status": status});
             });
         })
     });
