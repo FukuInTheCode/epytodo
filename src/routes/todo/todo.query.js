a25565e9dbde4bebeeafedf9e1e897c8224041d4
Tom C <tom.carlier@epitech.eu>
2024-05-14 10:30:36 +0200

    [refactor] -->  add the get_todo_by_id and check_todo_by_id function without any code

--- src/routes/todo/todo.query.js
+++ src/routes/todo/todo.query.js
@@ -3,3 +3,11 @@ const db = require("../../config/db.js");
 exports.get_all_todos = (to_call) => {
     return to_call();
 };
+
+exports.check_todo_by_id = (id, to_call) => {
+    return to_call(84);
+};
+
+exports.get_todo_by_todo = (id, to_call) => {
+    return to_call(84);
+};
