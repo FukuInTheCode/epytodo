53f9c073fe1bb00a26e31f9d6431c8284e35453f
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-14 09:58:36 +0200

    [fix] --> correction of Tom Carlier's errors there

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -1,4 +1,7 @@
 const auth = require("../../middlewares/auth.js");
+const {get_all_user_todos, check_user_by_id, check_user_by_email, get_user_by_id, get_user_by_email, update_user_by_id,
+    delete_user_by_id, get_all_users
+} = require("./user.query");
 
 module.exports = (app, bcrypt) => {
     app.get("/user", auth, (req, res) => {
@@ -27,10 +30,10 @@ module.exports = (app, bcrypt) => {
 
     app.get("/user/:id", auth, (req, res) => {
         check_user_by_id(req.params.id, (exist) => {
-            if (exist == 84) {
+            if (exist === 84) {
                 res.status(500).json({"msg": "Internal server error"});
                 return;
-            };
+            }
             if (!exist) {
                 res.status(404).json({"msg": "Not found"});
                 return;
@@ -47,10 +50,10 @@ module.exports = (app, bcrypt) => {
 
     app.get("/user/:email", auth, (req, res) => {
         check_user_by_email(req.params.email, (exist) => {
-            if (exist == 84) {
+            if (exist === 84) {
                 res.status(500).json({"msg": "Internal server error"});
                 return;
-            };
+            }
             if (!exist) {
                 res.status(404).json({"msg": "Not found"});
                 return;
@@ -69,24 +72,24 @@ module.exports = (app, bcrypt) => {
         const email = req.body["email"];
         const firstname = req.body["firstname"];
         const name = req.body["name"];
-        const password = req.body["password"];
+        let password = req.body["password"];
 
         if (!email || !firstname || !name || !password) {
             res.status(498).json({"msg": "Bad parameter"});
             return;
         }
         check_user_by_id(req.params.id, (exist) => {
-            if (exist == 84) {
+            if (exist === 84) {
                 res.status(500).json({"msg": "Internal server error"});
                 return;
-            };
+            }
             if (!exist) {
                 res.status(404).json({"msg": "Not found"});
                 return;
             }
-            pwd = bcrypt.hashSync(pwd, 10);
+            password = bcrypt.hashSync(password, 10);
             update_user_by_id(req.params.id, email, password, name, firstname, (err) => {
-                if (err == 84) {
+                if (err === 84) {
                     res.status(500).json({"msg": "Internal server error"});
                     return;
                 }
@@ -105,16 +108,16 @@ module.exports = (app, bcrypt) => {
         const id = req.params.id;
 
         check_user_by_id(id, (exist) => {
-            if (exist == 84) {
+            if (exist === 84) {
                 res.status(500).json({"msg": "Internal server error"});
                 return;
-            };
+            }
             if (!exist) {
                 res.status(404).json({"msg": "Not found"});
                 return;
             }
             delete_user_by_id(id, (err) => {
-                if (err == 84) {
+                if (err === 84) {
                     res.status(500).json({"msg": "Internal server error"});
                     return;
                 }
