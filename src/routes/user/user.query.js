c7bbddd35e5d6955cdf22e7442d18de37e4a8fef
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-14 10:18:00 +0200

    [feat] -->  added the function for check if a user exist with an email

--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -42,7 +42,17 @@ exports.get_user_by_id = (id, to_call) => {
 };
 
 exports.check_user_by_email = (email, to_call) => {
-    return to_call(84);
+    db.query("SELECT COUNT(*) AS count FROM user WHERE email = ?", [email], (err, result) => {
+        if (err)
+            to_call(84);
+        else {
+            const count = result[0].count;
+            if (count > 0)
+                to_call(1);
+            else
+                to_call(0);
+        }
+    });
 };
 
 exports.get_user_by_email = (email, to_call) => {
