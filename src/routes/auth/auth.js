acd85bc3c176cb1235605a40e67bc92a980e7825
Tom C <tom.carlier@epitech.eu>
2024-05-17 09:30:15 +0200

    [fix] --> fix auth route

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -1,29 +1,36 @@
 const { check_user_by_email, create_user, get_mail_account } = require("../user/user.query.js");
 const jwt = require("jsonwebtoken");
-const is_validMail = email => /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,10})+$/.test(email.toLowerCase());
+function isValidEmail(email: string) {
+  return email
+      .toLowerCase()
+      .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+}
+
 
 module.exports = function(app, bcrypt) {
     app.post('/login', (req, res) => {
-        var mail = req.body["email"];
+        var mail = req.body.email;
 
-        if (mail === undefined || req.body["password"] === undefined || !is_validMail(mail)) {
+        if (mail === undefined || req.body.email === undefined) {
             res.status(400).json({"msg": "Bad parameter"});
             return;
         }
-        get_mail_account(res, mail, req.body["password"], bcrypt, function(nbr) {
+        get_mail_account(res, mail, req.body.password, bcrypt, function(nbr) {
             if (nbr == 84) {
                 res.status(401).json({"msg":"Invalid Credentials"})
                 return;
             }
-            res.status(200).json({"token": jwt.sign({email: mail, password: req.body["password"]}, process.env.SECRET)})
+            res.status(200).json({"token": jwt.sign({email: mail, password: req.body.password}, process.env.SECRET)})
         });
     });
 
     app.post("/register", (req, res) => {
-        var email = req.body["email"];
-        var firstname = req.body["firstname"];
-        var name = req.body["name"];
-        var password = req.body["password"];
+        var email = req.body.email;
+        var firstname = req.body.firstname;
+        var name = req.body.name;
+        var password = req.body.password;
         if (!email || !firstname || !name || !password || !is_validMail(email)) {
             res.status(400).json({"msg": "Bad parameter"});
             return;
