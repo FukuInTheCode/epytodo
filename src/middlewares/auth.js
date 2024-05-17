b4f0075ada320becddd2b676481cbfc6befaf09f
Tom C <tom.carlier@epitech.eu>
2024-05-17 09:18:25 +0200

    [fix] --> fix auth.js middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,13 +4,16 @@ module.exports = (req, res, next) => {
     const header = req.headers.authorization;
 
     if (!header) {
-        res.status(498).json({"msg": "No token , authorization denied"});
+        res.status(401).json({"msg": "No token , authorization denied"});
         return;
+    }
+    if (!header.split(' ')[0] || header.split(' ')[0] !== 'Bearer') {
+        return res.status(401).json({ msg: 'Token is not valid' });
     }
     const token = header.split(' ')[1];
     jwt.verify(token, process.env.SECRET, (err, user) => {
         if (err) {
-            res.status(498).json({"msg": "Token is not valid"});
+            res.status(401).json({"msg": "Token is not valid"});
             return;
         }
         req.user_email = user["email"];
